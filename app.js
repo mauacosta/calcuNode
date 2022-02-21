65c70a9684ac6239f5148397f11857f50620b6d2
Mau Acosta <chafito@live.com>
2022-02-20 21:53:54 -0600

    Recibe input de usuario y resuelve la operación

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 const mate = require("./mate");
 
-console.log(mate);
+console.log(
+  "Bienvenido a calculadora. Primero selecciona dos números y después una operación."
+);
+
+const operaciones = Object.values(mate);
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Inserta un número \n", (num1) => {
+  readline.question("Ingresa otro número \n", (num2) => {
+    let a = parseInt(num1);
+    let b = parseInt(num2);
+    console.log(`Selecciona una operación:
+    0. Suma
+    1. Resta
+    2. Multiplicación
+    3. División 
+    4. Modulo
+    `);
+    readline.question(
+      "Selecciona el número de la operación \n",
+      (seleccion) => {
+        let operacion = operaciones[seleccion];
+        let res = operacion(a, b);
+        console.log(`El resultado de la operación ${seleccion} es ${res}`);
+        readline.close();
+      }
+    );
+  });
+});
