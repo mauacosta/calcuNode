92fb1aeb81c28da96fa7eff55a9b8c21715fc8c3
Mau Acosta <chafito@live.com>
2022-02-24 20:40:49 -0600

    Interacción con captura de errores

--- app.js
+++ app.js
@@ -4,32 +4,39 @@ console.log(
   "Bienvenido a calculadora. Primero selecciona dos números y después una operación."
 );
 
-const operaciones = Object.values(mate);
+const operaciones = {
+  "+": mate.suma,
+  "-": mate.resta,
+  "*": mate.producto,
+  "/": mate.division,
+  "%": mate.modulo,
+};
 
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question("Inserta un número \n", (num1) => {
-  readline.question("Ingresa otro número \n", (num2) => {
-    let a = parseInt(num1);
-    let b = parseInt(num2);
-    console.log(`Selecciona una operación:
-    0. Suma
-    1. Resta
-    2. Multiplicación
-    3. División 
-    4. Modulo
-    `);
-    readline.question(
-      "Selecciona el número de la operación \n",
-      (seleccion) => {
-        let operacion = operaciones[seleccion];
-        let res = operacion(a, b);
-        console.log(`El resultado de la operación ${seleccion} es ${res}`);
+const calculadora = () => {
+  readline.question("Inserta un número \n", (num1) => {
+    readline.question("Ingresa otro número \n", (num2) => {
+      let a = parseInt(num1);
+      let b = parseInt(num2);
+      console.log(`Selecciona una operación`);
+      readline.question("(+, -, *, /, %) \n", (seleccion) => {
+        try {
+          let operacion = operaciones[seleccion];
+          let res = operacion(a, b);
+          console.log(`${num1} ${seleccion} ${num2} = ${res}`);
+        } catch (error) {
+          console.log(
+            "Error con la operación introducida. ¡Intentalo de nuevo!"
+          );
+        }
         readline.close();
-      }
-    );
+      });
+    });
   });
-});
+};
+
+calculadora();
